ba64594889d48b541c496b30611029b3468abb27
JeeJeeWee <jonah.weideveld@gmail.com>
2020-02-11 20:13:42 +0100

    eerste comments en methods

---
+++ snake.py
@@ -0,0 +1,17 @@
+class Snake:
+    # Alles wat de snake kan, wordt een method
+
+    def move(self):
+        pass
+
+    def grow(self):
+        pass
+
+    # method voor kleur veranderen
+
+    # method voor levens verliezen
+
+# class voor eten aanmaken
+
+# class voor muur
+
